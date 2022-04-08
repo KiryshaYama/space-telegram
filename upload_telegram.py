082c2d3e53fe248959f2a146174e25717971e31f
KiryshaYama <79928594+KiryshaYama@users.noreply.github.com>
2022-04-08 16:45:05 +0500

    added auto-posting to telegram channel
    posting once in day

--- upload_telegram.py
+++ upload_telegram.py
@@ -1,9 +1,14 @@
 import os
+import time
+import random
 
 from dotenv import load_dotenv
 load_dotenv()
 
-
 import telegram
 bot = telegram.Bot(token=os.environ['TELEGRAM_SPACE_API'])
-bot.send_message(chat_id='@Space_pictures_original', text="I'm sorry Dave I'm afraid I can't do that.")
+posting = True
+while posting == True:
+    DIR = './images'
+    bot.send_photo(chat_id='@Space_pictures_original', photo=open(os.path.join(DIR, random.choice(os.listdir(DIR))), 'rb')) #('tests/test.png', 'rb'))
+    time.sleep(86400)
