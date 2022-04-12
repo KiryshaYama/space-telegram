ea229669be9c7688abb978ea23f034e8dbe7ffa9
KiryshaYama <79928594+KiryshaYama@users.noreply.github.com>
2022-04-12 20:37:41 +0500

    reworked def get_file_path

--- download.py
+++ download.py
@@ -15,14 +15,10 @@ def check_for_errors(response):
 def download_image(img_url, folder_name):
     response = requests.get(img_url)
     response.raise_for_status()
-    check_for_errors(response)
     filename = os.path.split(urllib.parse.urlsplit(
         urllib.parse.unquote(img_url)).path)[1]
-    filepath = get_file_path(folder_name, filename)
+    Path(folder_name).mkdir(parents=True, exist_ok=True)
+    filepath = os.path.join(folder_name, filename)
     with open(filepath, 'wb') as file:
         file.write(response.content)
 
-
-def get_file_path(folder_name, filename):
-    Path(folder_name).mkdir(parents=True, exist_ok=True)
-    return os.path.join(folder_name, filename)
