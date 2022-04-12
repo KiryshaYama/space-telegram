9587681f846a7be865ada70f3bbd4a6f33815e22
KiryshaYama <79928594+KiryshaYama@users.noreply.github.com>
2022-04-12 21:18:21 +0500

    reworked func

--- upload_telegram.py
+++ upload_telegram.py
@@ -5,12 +5,17 @@ import telegram
 
 from dotenv import load_dotenv
 
-load_dotenv()
-bot = telegram.Bot(token=os.environ['TELEGRAM_SPACE_API'])
-posting = True
-while posting is True:
-    DIR = './images'
-    bot.send_photo(chat_id='@Space_pictures_original',
-                   photo=open(os.path.join(
-                       DIR, random.choice(os.listdir(DIR))), 'rb'))
-    time.sleep(int(os.environ['POSTING_PERIOD']))
+
+def main():
+    load_dotenv()
+    bot = telegram.Bot(token=os.environ['TELEGRAM_SPACE_API'])
+    while True:
+        DIR = './images'
+        bot.send_photo(chat_id='@Space_pictures_original',
+                       photo=open(os.path.join(
+                           DIR, random.choice(os.listdir(DIR))), 'rb'))
+        time.sleep(int(os.environ['POSTING_PERIOD']))
+
+
+if __name__ == '__main__':
+    main()
