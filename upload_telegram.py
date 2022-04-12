5a537d0ab5ea6cc3fb8a8cd31762791205d020fd
KiryshaYama <79928594+KiryshaYama@users.noreply.github.com>
2022-04-12 21:39:22 +0500

    Update upload_telegram.py
    added argparse
    added post frequency in seconds to args
    added chat_id to environ

--- upload_telegram.py
+++ upload_telegram.py
@@ -2,19 +2,33 @@ import os
 import time
 import random
 import telegram
+import argparse
 
 from dotenv import load_dotenv
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='Post photo to telegram channel'
+    )
+    parser.add_argument(
+        'posting_period',
+        type=int,
+        help='post frequency in seconds'
+    )
+    return parser.parse_args()
+
+
 def main():
     load_dotenv()
+    args = parse_arguments()
     bot = telegram.Bot(token=os.environ['TELEGRAM_SPACE_API'])
     while True:
         DIR = './images'
         with open(os.path.join(
                            DIR, random.choice(os.listdir(DIR))), 'rb') as photo:
-            bot.send_photo(chat_id='@Space_pictures_original', photo=photo)
-        time.sleep(int(os.environ['POSTING_PERIOD']))
+            bot.send_photo(os.environ['chat_id'], photo=photo)
+        time.sleep(args.posting_period)
 
 
 if __name__ == '__main__':
